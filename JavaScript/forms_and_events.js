38b0cac69ae915059281b10237b030d975e9f734
Sand <sand999.com>
2026-07-22 21:19:23 +0300

    Single handler for colors DONE

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -26,6 +26,7 @@ function setImage()
 function setBackgroundColor(event) 
 {
     document.body.style.backgroundColor = event.target.value;
+    console.log(event.target.e);
     //let color = document.getElementById("background-color").value;
     //document.body.style.backgroundColor = color;
     //alert("setBackground-color");
@@ -33,4 +34,21 @@ function setBackgroundColor(event)
 function setForegroundColor()
 {
     document.body.style.color = document.getElementById("foreground-color").value;
+}
+function setColor(event)
+{
+    /*
+    ---------------------------------------
+    == - сравнивает два значения;
+    === - сравнивает два значения и типы этих значений.
+    === - возвращает 'true' только в том случае,
+            если совпадают как значения, так и типы;
+    ---------------------------------------
+    */
+
+    if (event.target.id === 'background-color')
+        document.body.style.backgroundColor = event.target.value;
+    else
+        document.body.style.color = event.target.value;
+    console.log(event.target.ids);
 }
