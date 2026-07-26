da12f8bc19b49e0d5d25d7d4948b342d67b246f8
Sand <sand999.com>
2026-07-26 22:09:18 +0300

    Cheched ListClass

---
+++ AdditionalElement/AdditionalElement.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+
+const element = document.getElementById('myElement');
+//Добавление класса к элементу
+//element.classList.add('highlight');
+function toggleHighlight()
+{
+    element.classList.toggle('highlight');
+}
